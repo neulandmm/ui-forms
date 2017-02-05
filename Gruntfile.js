59b47874e47a589d1e79efbe80e3ff32cd1a8bec
Mathias Bauer <sourcetree@mathiasbauer.net>
2017-02-05 14:02:14 +0100

    basic package building setup

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        // Project settings
+        pkg: grunt.file.readJSON('package.json')
+    });
+
+
+    grunt.registerTask('default', [
+    ]);
+}
